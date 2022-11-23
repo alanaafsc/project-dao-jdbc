a71e0b1951e9fde53be60559dcfe5dc6571d4bb2
alanaafsc <alana.ingrid@ufpe.br>
2022-11-23 10:05:09 -0300

    Implementando delete

--- project-dao-jdbc/src/application/Program.java
+++ project-dao-jdbc/src/application/Program.java
@@ -2,6 +2,9 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +14,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -46,6 +51,14 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
+		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 
 	}
 
